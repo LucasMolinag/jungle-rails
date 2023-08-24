097ea931884f099a7590c35ff7c26f1dcc6d46f7
Lucas Molina <molinag.lucas@gmail.com>
2023-08-24 16:41:04 -0700

    Implement and secure dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = Product.count
+    @category_count = Category.count 
   end
+
+  before_action :authenticate, only: [:show]
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "Jungle" && password == "book"
+  end
+end
 end
